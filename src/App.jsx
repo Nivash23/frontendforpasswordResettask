b04ef7b761bf82b80da15335cf085be072baed3e
Nivash23 <nivashsubash32@gmail.com>
2023-12-24 19:23:03 +0530

    after added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,87 @@
 import React, { useState } from "react";
+import './styles/App.css'
+
 
 function userReg() {
   const [registerFormdata, setRegisterFormdata] = useState({
     username: "",
-    password: ""
+    password: "",
   });
 
   const handleRegister = async (e) => {
     e.preventDefault();
 
     const registerbody = {
-
       username: registerFormdata.username,
-      password: registerFormdata.password
+      password: registerFormdata.password,
     };
 
-    const response = await fetch('https://backendforcapstone-cokw.onrender.com/api/users/',
+    const response = await fetch(
+      "https://backendforcapstone-cokw.onrender.com/api/users/",
       {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
-          
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(registerbody)
-    
-      });
-  const data = await response.json();
-  
-  if (response.status == 200) {
-      console.log('user created sucessfully..');
+        body: JSON.stringify(registerbody),
+      }
+    );
+    const data = await response.json();
+
+    if (response.status == 200) {
+      console.log("user created sucessfully..");
       console.log(data);
       setRegisterFormdata({
         username: "",
-        password: ""
+        password: "",
       });
-  }
-  else {
-    console.log('Error');
-    console.log(data);
-  }
-}
-  
-
-
+      alert('user Registered sucessfully');
+    } else {
+      console.log("Error");
+      console.log(data);
+    }
+  };
 
   return (
-    <div>
+    <div id='container'>
+
+    <div >
       <h1>REGISTRATION PAGE</h1>
       <form onSubmit={handleRegister}>
-        <div>
-
-        <label>Username :</label>
-          <input type='email'
+        <div id='username'>
+          <label>Username :</label>
+          <input
+            type="email"
             placeholder="Email..."
             value={registerFormdata.username}
             required
-            onChange={(e) => setRegisterFormdata({ ...registerFormdata, username: e.target.value })} />
+            onChange={(e) =>
+              setRegisterFormdata({
+                ...registerFormdata,
+                username: e.target.value,
+              })
+            }
+          />
         </div>
-        <div>
+        <div  id='password' >
           <label>Password :</label>
-          <input type='password'
+          <input
+            type="password"
             placeholder="password..."
             value={registerFormdata.password}
             required
-            onChange={(e) => setRegisterFormdata({ ...registerFormdata, password: e.target.value })} />
+            onChange={(e) =>
+              setRegisterFormdata({
+                ...registerFormdata,
+                password: e.target.value,
+              })
+            }
+          />
         </div>
-        <button type='submit'>Register</button>
+        <button type="submit" >Register</button>
       </form>
     </div>
-    
-)
+    </div>
+  );
 }
 export default userReg;
